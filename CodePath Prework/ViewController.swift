1aa6251e6527d28208507c75d992615320fb3fb9
Mario Barraza <mbarraza1024@gmail.com>
2025-05-12 00:13:26 -0500

    Added prework files to repository

---
+++ CodePath Prework/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CodePath Prework
+//
+//  Created by Mario Barraza on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
